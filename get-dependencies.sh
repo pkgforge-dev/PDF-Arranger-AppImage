0022e7a9bbaada25e1e9569256dedd71a9e48f9c
Samueru-sama <xdglawyer@outlook.com>
2026-04-01 04:53:32 -0400

    package app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm pdfarranger
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,12 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/256x256/apps/com.github.jeromerobert.pdfarranger.png
+export DESKTOP/usr/share/applications/com.github.jeromerobert.pdfarranger.desktop
+export DEPLOY_SYS_PYTHON=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/pdfarranger /usr/lib/libgtk-3.so*
 
 # Additional changes can be done in between here
 
