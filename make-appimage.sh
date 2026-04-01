739dcb4f8633dbf6f070486fc10bace74baf95d2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-04-01 07:21:45 -0400

    fix

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,7 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/com.github.jeromerobert.pdfarranger.png
-export DESKTOP/usr/share/applications/com.github.jeromerobert.pdfarranger.desktop
+export DESKTOP=/usr/share/applications/com.github.jeromerobert.pdfarranger.desktop
 export DEPLOY_SYS_PYTHON=1
 
 # Deploy dependencies
